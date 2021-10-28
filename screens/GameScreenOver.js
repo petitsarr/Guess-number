9e4472e7637aeae5d3ac53cdb91ff0dbef75c465
petit <fayembad@gmail.com>
2021-10-29 01:24:26 +0200

    styling_img

--- screens/GameScreenOver.js
+++ screens/GameScreenOver.js
@@ -4,8 +4,8 @@ import { View, Text, StyleSheet, Button ,Image } from 'react-native';
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
-      <Text style= {{fontSize : 18}}>Le Jeu est Terminé</Text>
-      <Image source= {require('../assets/img.png')}/>
+      <Text style= {{fontSize : 30}}>Le Jeu est Terminé</Text>
+      <Image source= {require('../assets/img.png')} style= {styles.image}/>
       <Text  style= {{fontSize : 18}}>Le nombre de rounds est: {props.guessrounds}</Text>
       <Text  style= {{fontSize : 18}}>Le Numéro était : {props.userNumber}</Text>
       <Button title="NEW GAME" onPress={props.NewGame} />
@@ -18,6 +18,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  } ,
+  image : {
+ width : "80%" ,
+ height : 300  ,
+ borderRadius : 700
   }
 });
 
